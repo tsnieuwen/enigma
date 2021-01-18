ba8dd3ed2805e742c331819dc45d6c29b4fe7217
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-17 18:59:22 -0700

    Test cipher offsets

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,12 +1,14 @@
 class Cipher
   attr_reader :key_to_integer,
+              :date_to_integer,
               :a_shift,
               :b_shift,
               :c_shift,
               :d_shift
 
-  def initialize(key_string)
+  def initialize(key_string, date_string)
     @key_to_integer = key_string.to_i
+    @date_to_integer = date_string.to_i
     @a_shift = nil
     @b_shift = nil
     @c_shift = nil
@@ -27,6 +29,26 @@ class Cipher
 
   def d_key
     (@key_to_integer.digits.reverse[3].to_s + @key_to_integer.digits.reverse[4].to_s).to_i
+  end
+
+  def date_squared
+    @date_to_integer ** 2
+  end
+
+  def a_offset
+    date_squared.digits.reverse[-4]
+  end
+
+  def b_offset
+    date_squared.digits.reverse[-3]
+  end
+
+  def c_offset
+    date_squared.digits.reverse[-2]
+  end
+
+  def d_offset
+    date_squared.digits.reverse[-1]
   end
 
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -8,13 +8,14 @@ require './lib/cipher'
 class CipherTest < Minitest::Test
 
   def test_it_exists
-    cipher = Cipher.new('12487')
+    cipher = Cipher.new('12487', '120395')
     assert_instance_of Cipher, cipher
   end
 
   def test_it_has_readable_attributes
-    cipher = Cipher.new('12487')
+    cipher = Cipher.new('12487', '120395')
     assert_equal 12487, cipher.key_to_integer
+    assert_equal 120395, cipher.date_to_integer
     assert_nil cipher.a_shift
     assert_nil cipher.b_shift
     assert_nil cipher.c_shift
@@ -22,11 +23,20 @@ class CipherTest < Minitest::Test
   end
 
   def test_keys
-    cipher = Cipher.new('12487')
+    cipher = Cipher.new('12487', '120395')
     assert_equal 12, cipher.a_key
     assert_equal 24, cipher.b_key
     assert_equal 48, cipher.c_key
     assert_equal 87, cipher.d_key
+  end
+
+  def test_offsets
+    cipher = Cipher.new('12487', '120395')
+    assert_equal 14494956025, cipher.date_squared
+    assert_equal 6, cipher.a_offset
+    assert_equal 0, cipher.b_offset
+    assert_equal 2, cipher.c_offset
+    assert_equal 5, cipher.d_offset
   end
 
 end
