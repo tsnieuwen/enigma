6388cc954736d29dce46c47ff38438b26a812d6f
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-18 11:31:39 -0700

    Refactor encryper to take cryptable module into account

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -1,5 +1,7 @@
+require './lib/cryptable'
+
 class Encrypter
-  # include Cryptable
+  include Cryptable
   attr_reader :character_set,
               :cipher,
               :message
@@ -10,9 +12,9 @@ class Encrypter
     @message = message.downcase
   end
 
-  def split_message
-    @message.split("")
-  end
+  # def split_message
+  #   @message.split("")
+  # end
 
   # def character_set_hash
   #   new_hash = Hash.new
@@ -68,42 +70,42 @@ class Encrypter
   #   x
   # end
   #
-  # def shifted_message_indices_encrypt
-  #   i = -1
-  #   message_index.map do |element|
-  #     i += 1
-  #     if a_shift_indices.include?(i)
-  #       element + @cipher.a_shift
-  #     elsif b_shift_indices.include?(i)
-  #       element + @cipher.b_shift
-  #     elsif c_shift_indices.include?(i)
-  #       element + @cipher.c_shift
-  #     elsif d_shift_indices.include?(i)
-  #       element + @cipher.d_shift
-  #     end
-  #   end
-  # end
-  #
-  # def encrypted_indices
-  #   shifted_message_indices_encrypt.map do |index|
-  #     index % 27
-  #   end
-  # end
-  #
-  # def encrypted_characters
-  #   collector = []
-  #   encrypted_indices.each do |number|
-  #     character_set_hash.each do |character, index|
-  #       if index == number
-  #         collector << character
-  #       end
-  #     end
-  #   end
-  #   collector
-  # end
-  #
-  # def encrypted_message
-  #   encrypted_characters.join("")
-  # end
+  def shifted_message_indices_encrypt
+    i = -1
+    message_index.map do |element|
+      i += 1
+      if a_shift_indices.include?(i)
+        element + @cipher.a_shift
+      elsif b_shift_indices.include?(i)
+        element + @cipher.b_shift
+      elsif c_shift_indices.include?(i)
+        element + @cipher.c_shift
+      elsif d_shift_indices.include?(i)
+        element + @cipher.d_shift
+      end
+    end
+  end
+
+  def encrypted_indices
+    shifted_message_indices_encrypt.map do |index|
+      index % 27
+    end
+  end
+
+  def encrypted_characters
+    collector = []
+    encrypted_indices.each do |number|
+      character_set_hash.each do |character, index|
+        if index == number
+          collector << character
+        end
+      end
+    end
+    collector
+  end
+
+  def encrypted_message
+    encrypted_characters.join("")
+  end
 
 end
