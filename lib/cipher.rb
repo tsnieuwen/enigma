e475cf60a32b02efd7939b9b150df014cc1c4f6b
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-17 18:47:21 -0700

    Test keys a-d

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,16 +1,32 @@
 class Cipher
-  attr_reader :key_string,
+  attr_reader :key_to_integer,
               :a_shift,
               :b_shift,
               :c_shift,
               :d_shift
 
   def initialize(key_string)
-    @key_string = key_string
+    @key_to_integer = key_string.to_i
     @a_shift = nil
     @b_shift = nil
     @c_shift = nil
     @d_shift = nil
+  end
+
+  def a_key
+    (@key_to_integer.digits.reverse[0].to_s + @key_to_integer.digits.reverse[1].to_s).to_i
+  end
+
+  def b_key
+    (@key_to_integer.digits.reverse[1].to_s + @key_to_integer.digits.reverse[2].to_s).to_i
+  end
+
+  def c_key
+    (@key_to_integer.digits.reverse[2].to_s + @key_to_integer.digits.reverse[3].to_s).to_i
+  end
+
+  def d_key
+    (@key_to_integer.digits.reverse[3].to_s + @key_to_integer.digits.reverse[4].to_s).to_i
   end
 
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -14,11 +14,19 @@ class CipherTest < Minitest::Test
 
   def test_it_has_readable_attributes
     cipher = Cipher.new('12487')
-    assert_equal '12487', cipher.key_string
+    assert_equal 12487, cipher.key_to_integer
     assert_nil cipher.a_shift
     assert_nil cipher.b_shift
     assert_nil cipher.c_shift
     assert_nil cipher.d_shift
+  end
+
+  def test_keys
+    cipher = Cipher.new('12487')
+    assert_equal 12, cipher.a_key
+    assert_equal 24, cipher.b_key
+    assert_equal 48, cipher.c_key
+    assert_equal 87, cipher.d_key
   end
 
 end
