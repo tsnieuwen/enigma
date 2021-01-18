c2a4363d5badb2fafa441eeb0a1cd10e4545065d
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-17 18:21:44 -0700

    Test cipher has readable attributes

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,7 +1,16 @@
 class Cipher
+  attr_reader :key_string,
+              :a_shift,
+              :b_shift,
+              :c_shift,
+              :d_shift
 
   def initialize(key_string)
-    @key_string =key_string
+    @key_string = key_string
+    @a_shift = nil
+    @b_shift = nil
+    @c_shift = nil
+    @d_shift = nil
   end
 
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -12,4 +12,13 @@ class CipherTest < Minitest::Test
     assert_instance_of Cipher, cipher
   end
 
+  def test_it_has_readable_attributes
+    cipher = Cipher.new('12487')
+    assert_equal '12487', cipher.key_string
+    assert_nil cipher.a_shift
+    assert_nil cipher.b_shift
+    assert_nil cipher.c_shift
+    assert_nil cipher.d_shift
+  end
+
 end
